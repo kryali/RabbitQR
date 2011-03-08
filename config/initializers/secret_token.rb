52af5b82bfaf0594101226bc7749779814672f6e
Kiran Ryali <kiran.ryali@gmail.com>
2011-03-08 16:07:49 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QRPay::Application.config.secret_token = '2f00064d5a48c38c877e9ad4bb6d4ef316533a2a42de514b035fcf330c99c5454531e21fa69b07932a8b72cb9515ac40a9d89b81cbc5dbebdc2238af3834203e'
